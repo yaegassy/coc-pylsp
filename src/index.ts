78cd03a14ade506fbf711ecbeb7df61cc95f3c21
yaegassy <yaegassy@users.noreply.github.com>
2021-11-29 12:07:02 +0900

    feat: add tcp connection mode (default is "stdio") (#6)

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import {
 } from 'coc.nvim';
 
 import fs from 'fs';
+import net from 'net';
 
 import { pylspInstall } from './installer';
 import { existsPythonImportModule, getBuiltinToolPath, getCurrentPythonPath } from './tool';
@@ -27,6 +28,9 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
   const { subscriptions } = context;
   const pythonCommand = getCurrentPythonPath(extConfig);
+  const connectionMode = extConfig.get<string>('connectionMode', 'stdio');
+  const tcpHost = extConfig.get<string>('tcpHost', '127.0.0.1');
+  const tcpPort = extConfig.get<number>('tcpPort', 2087);
 
   // MEMO: Priority to detect pylsp
   //
@@ -35,39 +39,42 @@ export async function activate(context: ExtensionContext): Promise<void> {
   // 3. builtin pylsp
   let existsPylspModule = false;
   let pylspPath: string | undefined = extConfig.get('commandPath', '');
-  if (pylspPath) {
-    workspace.expand(pylspPath);
-    if (!fs.existsSync(pylspPath)) {
-      pylspPath = undefined;
-    }
-  }
-  if (!pylspPath) {
-    if (pythonCommand) {
-      if (await existsPythonImportModule(pythonCommand.env, 'pylsp')) {
-        pylspPath = 'dummy';
-        existsPylspModule = true;
-      }
 
-      if (!existsPylspModule) {
-        pylspPath = getBuiltinToolPath(context.storagePath, 'pylsp');
+  if (connectionMode === 'stdio') {
+    if (pylspPath) {
+      workspace.expand(pylspPath);
+      if (!fs.existsSync(pylspPath)) {
+        pylspPath = undefined;
       }
     }
-  }
+    if (!pylspPath) {
+      if (pythonCommand) {
+        if (await existsPythonImportModule(pythonCommand.env, 'pylsp')) {
+          pylspPath = 'dummy';
+          existsPylspModule = true;
+        }
 
-  // Install "pylsp" if it does not exist.
-  if (!pylspPath && !existsPylspModule) {
-    if (pythonCommand) {
-      await installWrapper(pythonCommand.real, context);
-    } else {
-      window.showErrorMessage('python3/python command not found');
+        if (!existsPylspModule) {
+          pylspPath = getBuiltinToolPath(context.storagePath, 'pylsp');
+        }
+      }
     }
-    pylspPath = getBuiltinToolPath(context.storagePath, 'pylsp');
-  }
 
-  // If "pylsp" does not exist completely, terminate the process.
-  if (!pylspPath) {
-    window.showErrorMessage('Exit because "pylsp" does not exist.');
-    return;
+    // Install "pylsp" if it does not exist.
+    if (!pylspPath && !existsPylspModule) {
+      if (pythonCommand) {
+        await installWrapper(pythonCommand.real, context);
+      } else {
+        window.showErrorMessage('python3/python command not found');
+      }
+      pylspPath = getBuiltinToolPath(context.storagePath, 'pylsp');
+    }
+
+    // If "pylsp" does not exist completely, terminate the process.
+    if (!pylspPath) {
+      window.showErrorMessage('Exit because "pylsp" does not exist.');
+      return;
+    }
   }
 
   // old command. It is kept for compatibility,
@@ -96,10 +103,13 @@ export async function activate(context: ExtensionContext): Promise<void> {
     })
   );
 
-  const serverOptions: ServerOptions = {
-    command: existsPylspModule ? 'pylsp' : pylspPath,
-    args: ['-vv'],
-  };
+  let serverOptions: ServerOptions;
+  if (connectionMode === 'tcp') {
+    serverOptions = useLanguageServerOverTCP(tcpHost, tcpPort);
+  } else {
+    const lsCommand = existsPylspModule ? 'pylsp' : pylspPath;
+    serverOptions = useLanguageServerOverStdio(lsCommand);
+  }
 
   const clientOptions: LanguageClientOptions = {
     documentSelector: ['python'],
@@ -114,6 +124,28 @@ export async function activate(context: ExtensionContext): Promise<void> {
   const client = new LanguageClient('pylsp', 'Python lsp server', serverOptions, clientOptions);
 
   subscriptions.push(services.registLanguageClient(client));
+}
+
+function useLanguageServerOverTCP(host: string, port: number): ServerOptions {
+  return () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return new Promise((resolve, reject) => {
+      const clientSocket = new net.Socket();
+      clientSocket.connect(port, host, () => {
+        resolve({
+          reader: clientSocket,
+          writer: clientSocket,
+        });
+      });
+    });
+  };
+}
+
+function useLanguageServerOverStdio(lsCommand: string): ServerOptions {
+  return {
+    command: lsCommand,
+    args: ['-vv'],
+  };
 }
 
 async function installWrapper(pythonCommand: string, context: ExtensionContext) {
