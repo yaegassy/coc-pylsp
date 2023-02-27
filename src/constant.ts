1d9ffb9ddd8d2e753ba7846f1ceecebfe37548d1
yaegassy <yaegassy@users.noreply.github.com>
2023-02-27 12:30:02 +0900

    refactor: use pyproject.toml in the built-in installer (#13)

--- src/constant.ts
+++ src/constant.ts
@@ -1,10 +1,25 @@
-/**
- * System-wide constants.
- *
- * May be read from package.json
- */
+import fs from 'fs';
+import path from 'path';
+import toml from 'toml';
 
-// @ts-ignore
-import { pylspVersion } from '../package.json';
+type PyprojectToml = {
+  tool: {
+    poetry: {
+      dependencies: {
+        [name: string]: string;
+      };
+    };
+  };
+};
 
-export const PYLSP_VERSION = pylspVersion;
+function getPackageVersion(name: string) {
+  const rootDir = path.resolve(path.dirname(__filename), '..');
+  const filePath = path.join(rootDir, 'pyproject.toml');
+  const fileStr = fs.readFileSync(filePath);
+  const data: PyprojectToml = toml.parse(fileStr.toString());
+  const version = data.tool.poetry.dependencies[name];
+
+  return version;
+}
+
+export const PYLSP_VERSION = getPackageVersion('python-lsp-server');
